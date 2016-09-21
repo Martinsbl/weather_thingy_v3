146af67a079df96354923d22c4a634342806c189
martin <martinsbl@gmail.com>
2016-09-21 22:02:21 +0200

    .map file

--- toolchain.cmake
+++ toolchain.cmake
@@ -9,7 +9,7 @@ add_compile_options(-Wno-deprecated-declarations) # Get rid of "The CMAKE_FORCE_
 SET(CMAKE_C_FLAGS "-std=gnu99 -mthumb -mabi=aapcs -g3 -mfloat-abi=soft -ffunction-sections -fdata-sections -fno-strict-aliasing -fno-builtin --short-enums")
 SET(CMAKE_ASM_FLAGS "-x assembler-with-cpp")
 
-SET(LINKER_SCRIPT "/home/martin/ClionProjects/weather_thingy_v3/linker_nrf52.ld")
+SET(LINKER_SCRIPT "/home/martin/nordic/SDK11/examples/weather_thingy_v4/linker_nrf51.ld")
 SET(TEMPLATE_PATH "/home/martin/nordic/SDK11/components/toolchain/gcc/Makefile.posix")
-SET(CMAKE_EXE_LINKER_FLAGS "-Xlinker -Map=${OUTPUT_FILENAME}.map -mthumb -mabi=aapcs -L ${TEMPLATE_PATH} -T${LINKER_SCRIPT} -Wl,--gc-sections  --specs=nano.specs -lc -lnosys")
+SET(CMAKE_EXE_LINKER_FLAGS "-Xlinker -Map=${PROJECT_SOURCE_DIR}/${PROJECT_NAME}.map -mthumb -mabi=aapcs -L ${TEMPLATE_PATH} -T${LINKER_SCRIPT} -Wl,--gc-sections  --specs=nano.specs -lc -lnosys")
 
